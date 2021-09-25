777deb218250d1224158f9aa3c8daf19273800f1
Md.Minhazul Haque <ashim.ashim7470@gmail.com>
2021-09-26 00:44:42 +0600

    header section styles added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
     return (
         <div className='header'>
             <div className="header-text">
-                <h1>Zoo Arena</h1>
+                <h1 className='header-title'>Zoo Arena</h1>
                 <p>Choose your own animals from the collection of many animals in a budget and make your own zoo!</p>
                 <br />
-                <h3>Budget: $200000</h3>
+                <h3 className='header-budget'>Budget: $200000</h3>
             </div>
         </div>
     );
